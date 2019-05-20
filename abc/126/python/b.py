821879680e05a1383703ebd509c30c73189038d6
h-sakano <h.sakano@world-bit.co.jp>
2019-05-20 12:23:20 +0900

    B問題を回答

---
+++ abc/126/python/b.py
@@ -0,0 +1,19 @@
+def canMonth(value):
+    return 1 <= value and value <= 12
+
+
+S = input()
+
+F = int(S[:2])
+B = int(S[2:])
+fm = canMonth(F)
+bm = canMonth(B)
+
+if fm and bm:
+    print('AMBIGUOUS')
+elif not fm and not bm:
+    print('NA')
+elif fm:
+    print('MMYY')
+else:
+    print('YYMM')
