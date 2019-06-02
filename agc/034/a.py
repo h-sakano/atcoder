3c015bcde0df88ba9eba2484fa1ba0380da8b133
h-sakano <h.sakano@world-bit.co.jp>
2019-06-02 22:34:09 +0900

    a完成とbの途中

--- agc/034/a.py
+++ agc/034/a.py
@@ -1,1 +1,44 @@
 # -*- coding: utf-8 -*-
+n, a, b, c, d = map(int, input().split())
+s = input()
+grid = []
+for rock in s:
+    grid.append(rock == '#')
+
+if grid[c-1] or grid[d-1]:
+    print('No')
+
+max_rock_count = 0
+rock_count = 0
+if c < d:
+    for i in range(a-1, d):
+        if grid[i]:
+            rock_count += 1
+        else:
+            rock_count = 0
+
+        max_rock_count = max(max_rock_count, rock_count)
+
+    if max_rock_count >= 2:
+        print('No')
+    else:
+        print('Yes')
+elif c > d:
+    max_rock_count = 0
+    rock_count = 0
+    canSkip = False
+    for i in range(a-1, c):
+        if grid[i]:
+            rock_count += 1
+        else:
+            if b - 1 <= i and i <= c - 2 and not grid[i-1] and not grid[i] and not grid[i+1]:
+                if i-1 != d - 1:
+                    canSkip = True
+            rock_count = 0
+
+        max_rock_count = max(max_rock_count, rock_count)
+
+    if max_rock_count <= 1 and canSkip:
+        print('Yes')
+    else:
+        print('No')
--- agc/034/b.py
+++ agc/034/b.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+import re
+
+qS = input()
+
+counts = {}
+
+
+def replace(s, count):
+    if counts.get(s, -1) >= 0:
+        return counts[s]
+    else:
+        matches = re.finditer('ABC', s)
+        max_count = count
+        for m in matches:
+            m.span()
+            max_count = max(max_count, replace(
+                s[:m.span()[0]] + 'BCA' + s[m.span()[1]:], count + 1))
+        counts[s] = max_count
+        return counts[s]
+
+
+print(replace(qS, 0))
