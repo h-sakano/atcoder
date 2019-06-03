75d9ad7ca4350153c286473efe67bcebd3a26dfd
h-sakano <h.sakano@world-bit.co.jp>
2019-06-03 09:50:37 +0900

    文字を置き換えてfor文で回したほうが早い
    114ms => 47ms

--- agc/034/after/b.py
+++ agc/034/after/b.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 s = input()
+s = s.replace('BC', 'D')
 
 a_num = 0
 ans = 0
-i = 0
-while i < len(s):
-    if s[i] == 'A':
+for c in s:
+    if c == 'A':
         a_num += 1
-        i += 1
-    elif s[i:i+2] == 'BC':
+    elif c == 'D':
         ans += a_num
-        i += 2
     else:
         a_num = 0
-        i += 1
 
 print(ans)
