8b5bde7625f2fad1d66da667f11882292480530f
h-sakano <h.sakano@world-bit.co.jp>
2019-07-04 08:53:56 +0900

    GCDとLCMを追加し、ABC118を解く

---
+++ abc/118/c.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+ans = A[0]
+for i in range(1, N):
+    ans = gcd(ans, A[i])
+
+print(ans)
