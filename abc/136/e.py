2936e4d145427962859b7c7a63b71bbab448b8e8
h-sakano <h.sakano@world-bit.co.jp>
2019-08-05 10:39:29 +0900

    E問題復習

--- abc/136/e.py
+++ abc/136/e.py
@@ -1,2 +1,23 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
+
+sumA = sum(A)
+divisors = []
+for i in range(1, sumA + 1):
+    if i * i > sumA:
+        break
+    if sumA % i != 0:
+        continue
+    divisors.append(i)
+    if sumA // i != i:
+        divisors.append(sumA // i)
+
+divisors.sort(reverse=True)
+
+ans = 0
+for d in divisors:
+    costs = [a % d for a in A]
+    costs.sort()
+    if sum(costs[:N - sum(costs) // d]) <= K:
+        print(d)
+        break
