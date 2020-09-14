211a8008ce1dec4281c885d15130e2b130bf9a56
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-14 23:21:05 +0900

    ABC177のB, CをRustで解く

--- abc177/src/bin/b.rs
+++ abc177/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::{fastout, input, marker::Chars};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let n = s.len();
+    let m = t.len();
+    let mut ans = std::i64::MAX;
+
+    for i in 0..n - m + 1 {
+        let mut count = 0;
+        for j in 0..m {
+            if s[i + j] != t[j] {
+                count += 1;
+            }
+        }
+        if count < ans {
+            ans = count;
+        }
+    }
+
+    println!("{}", ans)
 }
--- abc177/src/bin/c.rs
+++ abc177/src/bin/c.rs
@@ -1,3 +1,22 @@
+use proconio::{fastout, input};
+
+const MOD: u64 = 1_000_000_007;
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u64; n],
+    }
+
+    let mut ans = 0;
+    let mut sum: u64 = a.iter().sum();
+
+    for i in a {
+        sum -= i;
+        ans += (sum % MOD) * (i % MOD) % MOD;
+        ans %= MOD;
+    }
+
+    println!("{}", ans)
 }
