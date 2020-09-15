04125c225acdbaaa6509ea1509b1729945f0d823
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-15 13:02:49 +0900

    ABC176のC

--- abc176/src/bin/c.rs
+++ abc176/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [u64; n],
+    }
+    let mut ans = 0;
+    for i in 0..n - 1 {
+        if a[i] > a[i + 1] {
+            let diff = a[i] - a[i + 1];
+            ans += diff;
+            a[i + 1] += diff;
+        }
+    }
+    println!("{}", ans)
 }
