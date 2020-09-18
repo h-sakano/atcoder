341d02db2b06305561739107bd0450a94f6f296f
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-18 13:59:31 +0900

    D問題を解く

--- abc174/src/bin/d.rs
+++ abc174/src/bin/d.rs
@@ -1,6 +1,27 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        c: Chars,
+    }
+
+    let mut ans = 0;
+
+    let mut l = 0;
+    let mut r = n - 1;
+    while r as i64 - l as i64 > 0 {
+        if c[l] == 'R' {
+            l += 1;
+        } else if c[r] == 'W' {
+            r -= 1;
+        } else if c[l] == 'W' && c[r] == 'R' {
+            ans += 1;
+            l += 1;
+            r -= 1;
+        }
+    }
+
+    println!("{}", ans)
 }
