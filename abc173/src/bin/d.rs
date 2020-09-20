9b51cc467de7978f5f41eb94d84b689eeae09b1a
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-20 10:53:16 +0900

    D問題を解いてみる

--- abc173/src/bin/d.rs
+++ abc173/src/bin/d.rs
@@ -1,6 +1,32 @@
 use proconio::{fastout, input};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut c: [usize; n],
+    }
+
+    c.sort();
+    c.reverse();
+
+    let mut edges = VecDeque::new();
+    edges.push_back(0usize);
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += match edges.pop_front() {
+            Some(t) => t,
+            None => 0,
+        };
+        if i == 0 {
+            edges.push_back(c[i])
+        } else {
+            edges.push_back(c[i]);
+            edges.push_back(c[i]);
+        }
+    }
+
+    println!("{}", ans);
 }
