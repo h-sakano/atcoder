a1dba82301aaf97805d3ebaa3c7b8b056462a39f
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-20 11:14:10 +0900

    D問題の最適化

--- abc173/src/bin/d.rs
+++ abc173/src/bin/d.rs
@@ -1,5 +1,4 @@
 use proconio::{fastout, input};
-use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
@@ -9,23 +8,10 @@ fn main() {
     }
 
     c.sort();
-    c.reverse();
-
-    let mut edges = VecDeque::new();
-    edges.push_back(0usize);
 
     let mut ans = 0;
-    for i in 0..n {
-        ans += match edges.pop_front() {
-            Some(t) => t,
-            None => 0,
-        };
-        if i == 0 {
-            edges.push_back(c[i])
-        } else {
-            edges.push_back(c[i]);
-            edges.push_back(c[i]);
-        }
+    for i in 1..n {
+        ans += c[n - 1 - i / 2]
     }
 
     println!("{}", ans);
