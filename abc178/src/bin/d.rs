441be61d5a1a47e137b2367ee74a6b1ebec99b7b
Hiroki Sakano <h.sakano26@gmail.com>
2020-09-22 09:24:49 +0900

    AtCoderのD問題

--- abc178/src/bin/d.rs
+++ abc178/src/bin/d.rs
@@ -1,6 +1,18 @@
 use proconio::{fastout, input};
 
+const MOD: usize = 1_000_000_007;
+
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: usize,
+    }
+
+    let mut dp = vec![0; s + 1];
+    dp[0] = 1;
+    for i in 3..=s {
+        dp[i] = (dp[i - 1] + dp[i - 3]) % MOD;
+    }
+
+    println!("{}", dp[s]);
 }
