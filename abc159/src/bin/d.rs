d64cca3f4aa2ebc6ca973808e6f9bf1080cac16a
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-04 14:19:21 +0900

    159のd問題

--- abc159/src/bin/d.rs
+++ abc159/src/bin/d.rs
@@ -1,6 +1,26 @@
 use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut dict = HashMap::new();
+    for i in 0..n {
+        let count = dict.entry(a[i]).or_insert(0i64);
+        *count += 1;
+    }
+
+    let mut counts = 0;
+    for (_, &value) in &dict {
+        counts += (value) * (value - 1) / 2;
+    }
+
+    for k in 0..n {
+        let count = dict.entry(a[k]).or_insert(0i64);
+        println!("{}", counts - *count + 1);
+    }
 }
