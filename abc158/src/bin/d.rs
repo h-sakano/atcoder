60108bc2d2d328f2bb9f6372915a53a21d608185
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-06 08:43:15 +0900

    ABC158のD問題

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -1,6 +1,47 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        q: usize,
+    }
+
+    let mut queue = VecDeque::from(s);
+    let mut reverse = false;
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+
+        if t == 1 {
+            reverse = !reverse;
+        } else {
+            input! {
+                f: usize,
+                c: char,
+            }
+
+            if f == 1 {
+                if reverse {
+                    queue.push_back(c);
+                } else {
+                    queue.push_front(c);
+                }
+            } else {
+                if reverse {
+                    queue.push_front(c);
+                } else {
+                    queue.push_back(c);
+                }
+            }
+        }
+    }
+
+    if reverse {
+        println!("{}", queue.iter().rev().collect::<String>())
+    } else {
+        println!("{}", queue.iter().collect::<String>())
+    }
 }
