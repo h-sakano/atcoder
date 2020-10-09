3f09d53e27f4aa59f178c60e531671aba4b9c08c
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-09 21:16:09 +0900

    abc156のbとc問題

--- abc156/src/bin/b.rs
+++ abc156/src/bin/b.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: f64,
+        k: f64,
+    }
+    println!("{}", (n.log(k).floor() + 1f64).floor());
 }
--- abc156/src/bin/c.rs
+++ abc156/src/bin/c.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: [isize; n],
+    }
+
+    let mut ans = std::isize::MAX;
+    for p in 0..100isize {
+        let mut sum = 0;
+        for i in 0..n {
+            sum += (x[i] - p).pow(2);
+        }
+        ans = std::cmp::min(ans, sum);
+    }
+
+    println!("{}", ans);
 }
