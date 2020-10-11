9369f269969605778544ad8cea25b4450107c118
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-11 09:43:41 +0900

    155のB

--- abc155/src/bin/b.rs
+++ abc155/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = "APPROVED";
+
+    for i in 0..n {
+        if a[i] % 2 == 0 && a[i] % 3 != 0 && a[i] % 5 != 0 {
+            ans = "DENIED";
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
