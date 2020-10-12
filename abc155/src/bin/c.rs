83c20a53194c13508f92c4b36d5b0db8ab979f7c
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-12 09:37:40 +0900

    abc155のC問題

--- abc155/src/bin/c.rs
+++ abc155/src/bin/c.rs
@@ -1,6 +1,30 @@
 use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut dict = HashMap::new();
+    let mut max = 0;
+    for word in s {
+        let count = dict.entry(word).or_insert(0);
+        *count += 1;
+        max = std::cmp::max(max, *count);
+    }
+
+    let mut max_list = vec![];
+    for (k, v) in &dict {
+        if *v == max {
+            max_list.push(k);
+        }
+    }
+
+    max_list.sort();
+    for v in max_list {
+        println!("{}", v);
+    }
 }
