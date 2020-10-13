a5f72c43991d7a99b0d912821b59109658086ca6
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-14 08:38:08 +0900

    abc154のd問題

--- abc154/src/bin/d.rs
+++ abc154/src/bin/d.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        p: [f64; n],
+    }
+    let mut exp = 0f64;
+    for i in 0..k {
+        exp += (p[i] + 1f64) / 2f64;
+    }
+
+    let mut max_exp = exp;
+    for i in 0..n - k {
+        exp = exp - (p[i] + 1f64) / 2f64 + (p[i + k] + 1f64) / 2f64;
+        max_exp = if exp > max_exp { exp } else { max_exp };
+    }
+
+    println!("{}", max_exp);
 }
