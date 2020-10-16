7de02e628083e42b01d3d2395718ef46a501f8a8
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-16 22:19:29 +0900

    abc153のd問題

--- abc153/src/bin/d.rs
+++ abc153/src/bin/d.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        mut h: usize,
+    }
+
+    let mut num = 1;
+    let mut ans = 0i64;
+    while h >= 1 {
+        h /= 2;
+        ans += num;
+        num *= 2;
+    }
+
+    println!("{}", ans);
 }
