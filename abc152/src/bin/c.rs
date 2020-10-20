79f1c6b16077b7a2e2228aaaaafe09752f35b56b
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-20 18:10:05 +0900

    ABC152のc問題

--- abc152/src/bin/c.rs
+++ abc152/src/bin/c.rs
@@ -1,12 +1,20 @@
-use num::integer::gcd;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        a: usize,
-        b: usize,
+        n: usize,
+        p: [usize; n],
     }
 
-    println!("{}", a * b / gcd(a, b));
+    let mut min = p[0];
+    let mut ans = 1;
+    for i in 1..n {
+        if p[i] <= min {
+            min = p[i];
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
