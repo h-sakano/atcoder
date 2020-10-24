3bf10c882d25730e1203a16952023c3b14bb37dd
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-24 09:15:38 +0900

    ABC149のD問題

--- abc149/src/bin/d.rs
+++ abc149/src/bin/d.rs
@@ -1,6 +1,42 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        r: usize,
+        s: usize,
+        p: usize,
+        t: Chars,
+    }
+
+    let mut ans = 0;
+    let mut history = vec![];
+    for i in 0..n {
+        if t[i] == 'r' {
+            if history.len() >= k && history[history.len() - k] == 'p' {
+                history.push('o');
+            } else {
+                history.push('p');
+                ans += p;
+            }
+        } else if t[i] == 's' {
+            if history.len() >= k && history[history.len() - k] == 'r' {
+                history.push('o');
+            } else {
+                history.push('r');
+                ans += r;
+            }
+        } else {
+            if history.len() >= k && history[history.len() - k] == 's' {
+                history.push('o');
+            } else {
+                history.push('s');
+                ans += s;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
