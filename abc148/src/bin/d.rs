351e6b4be2f62e5f75edbccd728790863dd500a6
Hiroki Sakano <h.sakano26@gmail.com>
2020-10-26 20:25:40 +0900

    ABC148のD問題

--- abc148/src/bin/d.rs
+++ abc148/src/bin/d.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut k = 0;
+    let mut now = 1;
+    for i in 0..n {
+        if a[i] == now {
+            now += 1;
+        } else {
+            k += 1;
+        }
+    }
+
+    if k == n {
+        println!("-1");
+    } else {
+        println!("{}", k);
+    }
 }
