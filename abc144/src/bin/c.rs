9b56518dfcf99fc7a68c5f89b11452a4e7553d29
Hiroki Sakano <h.sakano26@gmail.com>
2020-11-10 14:26:45 +0900

    abc144のC問題を解く

--- abc144/src/bin/c.rs
+++ abc144/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+    let mut i = 1;
+    let mut ans = std::usize::MAX;
+    while i * i <= n {
+        if n % i == 0 {
+            ans = std::cmp::min(ans, i - 1 + n / i - 1);
+        }
+        i += 1;
+    }
+
+    println!("{}", ans);
 }
