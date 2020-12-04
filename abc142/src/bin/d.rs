bff152d8f0c684a57005f71181e2ee60f3312c15
Hiroki Sakano <h.sakano26@gmail.com>
2020-12-04 18:09:04 +0900

    ABC142のD問題を解く

--- abc142/src/bin/d.rs
+++ abc142/src/bin/d.rs
@@ -1,6 +1,53 @@
+use num::integer::gcd;
 use proconio::{fastout, input};
+
+fn make_divisors(n: usize) -> Vec<usize> {
+    let mut lower_divisors = vec![];
+    let mut upper_divisors = vec![];
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            lower_divisors.push(i);
+            if i != n / i {
+                upper_divisors.push(n / i);
+            }
+        }
+        i += 1;
+    }
+    lower_divisors.append(&mut upper_divisors);
+    return lower_divisors;
+}
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    let da = make_divisors(a);
+    let mut cd = vec![];
+    for d in da {
+        if b % d == 0 {
+            cd.push(d);
+        }
+    }
+
+    cd.sort();
+    let n = cd.len();
+    let mut ans = 0;
+    for i in (0..n).rev() {
+        let mut ok = true;
+        for j in 0..i {
+            if gcd(cd[i], cd[j]) != 1 {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
