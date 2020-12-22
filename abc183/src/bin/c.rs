c16c7709f860c1f57166266af9904b85aa46de2c
Hiroki Sakano <h.sakano26@gmail.com>
2020-12-22 21:44:29 +0900

    ABC183-C

--- abc183/src/bin/c.rs
+++ abc183/src/bin/c.rs
@@ -1,6 +1,30 @@
+// 順列の問題
+
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        t: [[usize; n]; n],
+    }
+
+    let mut ans = 0;
+    for (_, v) in (1..=n - 1 as usize).permutations(n - 1).enumerate() {
+        let mut dist = 0;
+        let mut prev = 0;
+        for i in 0..n - 1 {
+            dist += t[prev][v[i]];
+            prev = v[i];
+        }
+        dist += t[prev][0];
+
+        if dist == k {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
