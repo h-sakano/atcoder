4d9b51c479d87331cef1ea00f5ceb791ddad5fc3
Hiroki Sakano <h.sakano26@gmail.com>
2020-12-24 13:20:09 +0900

    ABC182-D問題の修正

--- abc182/src/bin/d.rs
+++ abc182/src/bin/d.rs
@@ -9,29 +9,20 @@ fn main() {
 
     let mut totals = vec![];
     let mut count: i64 = 0;
+    let mut max_cursor: i64 = 0;
     for i in 0..n {
         count += a[i];
-        totals.push(count);
+        if count > max_cursor {
+            max_cursor = count;
+        }
+        totals.push((count, max_cursor));
     }
 
-    let mut max_cursor = -10i64.pow(9);
-    let mut max_index = 0;
     let mut cursor = 0;
-
-    for i in 1..=n {
-        cursor += totals[i - 1];
-        if cursor >= max_cursor {
-            max_cursor = cursor;
-            max_index = i;
-        }
-    }
-
-    cursor = max_cursor;
-    for i in 0..max_index {
-        cursor += a[i];
-        if cursor > max_cursor {
-            max_cursor = cursor;
-        }
+    let mut max_cursor = 0;
+    for i in 0..n {
+        max_cursor = std::cmp::max(cursor + totals[i].1, max_cursor);
+        cursor += totals[i].0;
     }
 
     println!("{}", max_cursor);
