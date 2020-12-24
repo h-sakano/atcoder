4f8f29822ce2c68a5401a82a7eec6537b23d412e
Hiroki Sakano <h.sakano26@gmail.com>
2020-12-24 09:25:30 +0900

    ABC182-Dを解く

--- abc182/src/bin/d.rs
+++ abc182/src/bin/d.rs
@@ -2,5 +2,37 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut totals = vec![];
+    let mut count: i64 = 0;
+    for i in 0..n {
+        count += a[i];
+        totals.push(count);
+    }
+
+    let mut max_cursor = -10i64.pow(9);
+    let mut max_index = 0;
+    let mut cursor = 0;
+
+    for i in 1..=n {
+        cursor += totals[i - 1];
+        if cursor >= max_cursor {
+            max_cursor = cursor;
+            max_index = i;
+        }
+    }
+
+    cursor = max_cursor;
+    for i in 0..max_index {
+        cursor += a[i];
+        if cursor > max_cursor {
+            max_cursor = cursor;
+        }
+    }
+
+    println!("{}", max_cursor);
 }
