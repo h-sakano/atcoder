77bd19ff1118df16994b3699b584d95af5d881e8
Hiroki Sakano <h.sakano26@gmail.com>
2020-12-29 17:40:15 +0900

    abc185のC問題

--- abc185/src/bin/c.rs
+++ abc185/src/bin/c.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        l: u128,
+    }
+
+    let mut numerator: u128 = 1;
+    let mut denominator: u128 = 1;
+    for i in 1..=11 {
+        numerator *= l - i as u128;
+        denominator *= i as u128;
+    }
+
+    println!("{}", numerator / denominator)
 }
