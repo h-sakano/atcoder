9de32900d6965667c8b9310eb41433a99b5dd4d1
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-02 12:36:00 +0900

    ABC186-D

--- abc186/src/bin/d.rs
+++ abc186/src/bin/d.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+    }
+
+    a.sort();
+    let mut ans = 0;
+    for i in 1..n {
+        ans += a[i] - a[0];
+    }
+    let mut prev = ans;
+    for i in 1..n {
+        let diff = a[i] - a[i - 1];
+        prev = prev - diff * ((n - i) as i64);
+        ans += prev;
+    }
+
+    println!("{}", ans);
 }
