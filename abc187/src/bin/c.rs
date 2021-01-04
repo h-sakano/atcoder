f548e23d3a606cc6007f43b4e10326387ac86eff
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-04 16:48:29 +0900

    ABC187-C

--- abc187/src/bin/c.rs
+++ abc187/src/bin/c.rs
@@ -1,6 +1,20 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        words: [String; n],
+    }
+
+    let words: HashSet<String> = words.into_iter().collect();
+    for word in &words {
+        if words.contains(&format!("{}{}", "!".to_string(), word)) {
+            println!("{}", word);
+            return;
+        }
+    }
+
+    println!("satisfiable");
 }
