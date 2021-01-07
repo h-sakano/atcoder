56bc65ea97646ac2610b263b5432c8ba037121e1
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-07 15:12:59 +0900

    ABC139-C

--- abc139/src/bin/c.rs
+++ abc139/src/bin/c.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: [usize; n],
+    }
+
+    let mut ans = 0;
+    let mut count = 0;
+    for i in 1..n {
+        if h[i] <= h[i - 1] {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        ans = std::cmp::max(ans, count);
+    }
+
+    println!("{}", ans);
 }
