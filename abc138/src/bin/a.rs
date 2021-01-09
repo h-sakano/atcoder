505c6f8aac7847c39739ecbf54cd7bcd8b56e73a
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-09 22:55:31 +0900

    ABC138のA問題

---
+++ abc138/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        a: usize,
+        s: String,
+    }
+
+    println!("{}", if a >= 3200 { s } else { "red".to_string() });
+}
