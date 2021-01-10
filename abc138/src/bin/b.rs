48efab23b4e3ef6b4f3cdc2f1b42a68e312faa8f
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-10 11:19:02 +0900

    ABC138-B

--- abc138/src/bin/b.rs
+++ abc138/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [f64; n],
+    }
+
+    let mut ans = 0.0;
+    for i in 0..n {
+        ans += 1.0 / a[i];
+    }
+    ans = 1.0 / ans;
+    println!("{}", ans);
 }
