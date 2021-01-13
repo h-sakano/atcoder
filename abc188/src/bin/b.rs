cffd15166dadbc4f5812a55b1f0961c342da8139
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-13 23:13:45 +0900

    ABC188-B

--- abc188/src/bin/b.rs
+++ abc188/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+        b: [i64; n],
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += a[i] * b[i];
+    }
+
+    println!("{}", if ans == 0 { "Yes" } else { "No" });
 }
