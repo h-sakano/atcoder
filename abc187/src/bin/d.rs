a9089e5a7b1b98258fcbe619f6f5d04561f6c679
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-17 08:46:31 +0900

    ABC187-D

--- abc187/src/bin/d.rs
+++ abc187/src/bin/d.rs
@@ -2,5 +2,28 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut ab: [(usize, usize); n],
+    }
+
+    // 高橋派と青木派(重み2)の数の合計の降順に並び替える
+    ab.sort_by(|x, y| (y.0 * 2 + y.1).cmp(&(x.0 * 2 + x.1)));
+    let mut aoki = 0;
+    let mut takahashi = 0;
+
+    for i in 0..n {
+        aoki += ab[i].0;
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        if takahashi <= aoki {
+            ans += 1;
+            aoki -= ab[i].0;
+            takahashi += ab[i].0 + ab[i].1;
+        }
+    }
+
+    println!("{}", ans);
 }
