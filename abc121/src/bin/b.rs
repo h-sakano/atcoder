c2a0947bd28e54dd63f652091890028ecb172bf8
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-16 16:15:10 +0900

    ABC121-B

--- abc121/src/bin/b.rs
+++ abc121/src/bin/b.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        c: i64,
+        b: [i64; m],
+        a: [[i64; m]; n],
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        let mut t = 0;
+        for j in 0..m {
+            t += a[i][j] * b[j];
+        }
+        t += c;
+        if t > 0 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
