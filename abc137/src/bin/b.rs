7df5a1cf94c38f6350762db357098e7a3f65ddfc
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-19 08:46:35 +0900

    ABC137-B

--- abc137/src/bin/b.rs
+++ abc137/src/bin/b.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        k: i64,
+        x: i64,
+    }
+
+    let mut ans = vec![];
+    for i in std::cmp::max(-1000000, x - k + 1)..=std::cmp::min(1000000, x + k - 1) {
+        ans.push(i);
+    }
+
+    println!(
+        "{}",
+        ans.into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
