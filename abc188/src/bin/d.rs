08f1a2c93e8448dcf0365d35603586d4040590b8
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-22 08:20:50 +0900

    ABC188-D

--- abc188/src/bin/d.rs
+++ abc188/src/bin/d.rs
@@ -2,5 +2,29 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        c: i64,
+        abc: [(usize, usize, i64); n],
+    }
+
+    let mut events = vec![];
+    for i in 0..n {
+        events.push((abc[i].0 - 1, abc[i].2));
+        events.push((abc[i].1, -abc[i].2));
+    }
+    events.sort();
+
+    let mut ans = 0;
+    let mut fee = 0i64;
+    let mut t = 0;
+    for (x, y) in events {
+        if x != t {
+            ans += std::cmp::min(c, fee) * (x - t) as i64;
+            t = x;
+        }
+        fee += y;
+    }
+
+    println!("{}", ans);
 }
