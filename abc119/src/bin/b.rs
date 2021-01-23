60e30a5b6b3d23cb318bf83a6e6c72fd4e9f28b8
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-24 08:09:13 +0900

    ABC119-B

--- abc119/src/bin/b.rs
+++ abc119/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xu: [(f64, String); n],
+    }
+
+    let mut ans = 0.0;
+    for i in 0..n {
+        ans += if xu[i].1 == "BTC" {
+            xu[i].0 * 380000.0
+        } else {
+            xu[i].0
+        };
+    }
+
+    println!("{}", ans);
 }
