66fc443573234efd005f1207283dff75fdc09a1f
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-26 17:24:24 +0900

    ABC189-B

--- abc189/src/bin/b.rs
+++ abc189/src/bin/b.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        vp: [(usize, usize); n],
+    }
+
+    let mut count = 0;
+    for i in 0..n {
+        count += vp[i].0 * vp[i].1;
+        if count > x * 100 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+
+    println!("-1");
 }
