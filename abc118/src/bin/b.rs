a308551b00f500fa6f5b81340f7b00a31c2560ca
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-28 23:26:00 +0900

    ABC118-B

--- abc118/src/bin/b.rs
+++ abc118/src/bin/b.rs
@@ -1,6 +1,30 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+    }
+
+    let mut count = vec![0; m];
+    for _ in 0..n {
+        input! {
+            k: usize,
+            a: [Usize1; k],
+        }
+
+        for j in 0..k {
+            count[a[j]] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..m {
+        if count[i] >= n {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
