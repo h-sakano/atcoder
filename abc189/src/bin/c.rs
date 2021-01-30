ab26af8078364b40d7f01b17b033a35c306e8725
Hiroki Sakano <h.sakano26@gmail.com>
2021-01-30 18:20:57 +0900

    ABC189-C

--- abc189/src/bin/c.rs
+++ abc189/src/bin/c.rs
@@ -2,5 +2,27 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    for x in 1..=10usize.pow(5) {
+        let mut now = 0;
+        let mut max = 0;
+
+        for i in 0..n {
+            if x <= a[i] {
+                now += x;
+            } else {
+                now = 0;
+            }
+            max = std::cmp::max(max, now);
+        }
+
+        ans = std::cmp::max(ans, max);
+    }
+
+    println!("{}", ans);
 }
