2ccfd8ee52f3e6e91880e1ed52c98efd98f2ae2e
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-01 10:57:50 +0900

    ABC190-B

--- abc190/src/bin/b.rs
+++ abc190/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        d: usize,
+        xy: [(usize, usize); n],
+    }
+
+    for i in 0..n {
+        if xy[i].0 < s && xy[i].1 > d {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
