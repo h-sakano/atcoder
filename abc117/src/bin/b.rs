0b38615f9feac0553274e3400e0aa0478e32923d
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-02 22:54:05 +0900

    ABC117-B

--- abc117/src/bin/b.rs
+++ abc117/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut l: [usize; n],
+    }
+
+    l.sort();
+    let sum: usize = l[0..n - 1].iter().sum();
+    if l[n - 1] < sum {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
