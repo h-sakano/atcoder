ba71e3d26fbf817a09e3890a8b0fb37da8be41f3
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-04 08:54:14 +0900

    ABC190-D

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        mut n: usize,
+    }
+
+    while n % 2 == 0 {
+        n /= 2;
+    }
+    let mut ans = 0;
+    let sq = (n as f64).sqrt() as usize;
+    for i in 1..=sq {
+        if n % i == 0 {
+            ans += 2;
+        }
+    }
+
+    if sq * sq == n {
+        ans -= 1;
+    }
+
+    println!("{}", ans * 2);
 }
