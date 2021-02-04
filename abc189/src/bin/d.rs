b0638a627817e31c3fc5ecc2c1310919181f737a
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-05 07:56:46 +0900

    ABC189-D

--- abc189/src/bin/d.rs
+++ abc189/src/bin/d.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut ans = 1;
+    for i in 0..n {
+        if s[i] == "OR" {
+            ans += 2usize.pow((i + 1) as u32);
+        }
+    }
+
+    println!("{}", ans);
 }
