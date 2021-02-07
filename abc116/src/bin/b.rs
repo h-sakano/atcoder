2bc42f128764230dc1aab754e775d3cbe10f8bc4
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-07 23:21:06 +0900

    ABC116-B

--- abc116/src/bin/b.rs
+++ abc116/src/bin/b.rs
@@ -2,5 +2,26 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: usize,
+    }
+
+    let mut memory = vec![false; 1_000_001];
+    let mut m = 1;
+    let mut t = s;
+    memory[t] = true;
+    loop {
+        if t % 2 == 0 {
+            t /= 2;
+        } else {
+            t = 3 * t + 1;
+        }
+        m += 1;
+        if memory[t] {
+            break;
+        }
+        memory[t] = true;
+    }
+
+    println!("{}", m);
 }
