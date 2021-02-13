9636f7ef0508ad6b694bff8a53e7f7d9090002f0
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-13 12:21:46 +0900

    HHKB2020-B

--- hhkb2020/src/bin/b.rs
+++ hhkb2020/src/bin/b.rs
@@ -1,6 +1,29 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        s: [Chars; h],
+    }
+
+    let mut ans = 0;
+    for j in 0..h {
+        for i in 0..w - 1 {
+            if s[j][i] == '.' && s[j][i + 1] == '.' {
+                ans += 1;
+            }
+        }
+    }
+
+    for i in 0..w {
+        for j in 0..h - 1 {
+            if s[j][i] == '.' && s[j + 1][i] == '.' {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
