947ed3bd0bc14ee3790123ffd6215fc961f3d70a
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-14 15:13:35 +0900

    HHKB2020-C(TLE)

--- hhkb2020/src/bin/c.rs
+++ hhkb2020/src/bin/c.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [usize; n],
+    }
+
+    let mut candidates = vec![];
+    for i in 0..=200_000 {
+        candidates.push(i);
+    }
+    for i in 0..n {
+        let index = match candidates.binary_search(&p[i]) {
+            Ok(x) => x as i64,
+            Err(_) => -1,
+        };
+
+        if index >= 0 {
+            candidates.remove(index as usize);
+        }
+
+        println!("{}", candidates[0]);
+    }
 }
