4857d7cf5b89f2ac0598452c8a8c58f1fef57be2
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-15 11:26:52 +0900

    ABC115-C

--- abc115/src/bin/c.rs
+++ abc115/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        mut h: [usize; n],
+    }
+
+    h.sort();
+    let mut ans = std::usize::MAX;
+    for i in 0..n - k + 1 {
+        ans = std::cmp::min(ans, h[i + k - 1] - h[i]);
+    }
+
+    println!("{}", ans);
 }
