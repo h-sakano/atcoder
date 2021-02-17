e411fb6a45b2cb0180b3fca6b0fa3c5b449caff8
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-17 09:44:15 +0900

    ABL-B

--- abl/src/bin/b.rs
+++ abl/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        d: i64,
+    }
+
+    println!(
+        "{}",
+        if std::cmp::min(b, d) - std::cmp::max(a, c) >= 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
