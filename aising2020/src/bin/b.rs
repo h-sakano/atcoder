594f58e16868871ae79ab255d9980fb831dc31b5
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-21 20:53:22 +0900

    aising2020-b

--- aising2020/src/bin/b.rs
+++ aising2020/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    for i in (0..n).step_by(2) {
+        if a[i] % 2 == 1 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
