6f1f86c56bffb75887d2030f8dcdf77b9fffd8e6
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-22 10:42:19 +0900

    aising2020-c

--- aising2020/src/bin/c.rs
+++ aising2020/src/bin/c.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut ans = vec![0; 100_000];
+    let n_sqrt = (n as f64).sqrt().ceil() as usize;
+
+    for x in 1..=n_sqrt {
+        for y in 1..=n_sqrt {
+            for z in 1..=n_sqrt {
+                ans[x * x + y * y + z * z + x * y + y * z + z * x] += 1;
+            }
+        }
+    }
+
+    for i in 0..n {
+        println!("{}", ans[i + 1]);
+    }
 }
