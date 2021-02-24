6c546fa76ea21d7436d24569805cf037cedc495b
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-24 13:53:25 +0900

    m-solutions-2020-B

--- m-solutions2020/src/bin/b.rs
+++ m-solutions2020/src/bin/b.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        mut b: usize,
+        mut c: usize,
+        mut k: usize,
+    }
+
+    while k > 0 && b <= a {
+        k -= 1;
+        b *= 2;
+    }
+
+    while k > 0 && c <= b {
+        k -= 1;
+        c *= 2;
+    }
+
+    println!("{}", if a < b && b < c { "Yes" } else { "No" });
 }
