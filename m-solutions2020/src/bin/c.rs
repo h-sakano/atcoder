cbd3c4d785a0ac3d53111da446514f09a523df3e
Hiroki Sakano <h.sakano26@gmail.com>
2021-02-25 09:00:28 +0900

    M-SOLUTIONS2020-C

--- m-solutions2020/src/bin/c.rs
+++ m-solutions2020/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    for i in k..n {
+        if a[i] > a[i - k] {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
