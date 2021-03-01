452c52ea80cb022e5330082fa648254228aacb7c
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-01 12:49:26 +0900

    M-SOLUTIONS2020-D

--- m-solutions2020/src/bin/d.rs
+++ m-solutions2020/src/bin/d.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut price = 1000;
+    for i in 0..n - 1 {
+        if a[i] < a[i + 1] {
+            price = a[i + 1] * (price / a[i]) + price % a[i];
+        }
+    }
+
+    println!("{}", price);
 }
