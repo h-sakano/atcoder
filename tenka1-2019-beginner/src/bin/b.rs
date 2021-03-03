52cc7f0b4846abfa6f335ea7a20b17d061f969f4
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-03 09:33:09 +0900

    tenka1-2019-beginner-b

--- tenka1-2019-beginner/src/bin/b.rs
+++ tenka1-2019-beginner/src/bin/b.rs
@@ -1,6 +1,21 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+        k: usize,
+    }
+
+    let mut ans = "".to_string();
+    for i in 0..n {
+        if s[i] == s[k - 1] {
+            ans += &s[i].to_string();
+        } else {
+            ans += "*";
+        }
+    }
+
+    println!("{}", ans);
 }
