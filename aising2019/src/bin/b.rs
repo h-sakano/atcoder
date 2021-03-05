6f8ddf09ea1bcbb8d9c1d8fcc94ac192dbb89818
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-05 23:24:01 +0900

    aising2019-b

--- aising2019/src/bin/b.rs
+++ aising2019/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        p: [usize; n],
+    }
+
+    let mut count = vec![0; 3];
+    for i in 0..n {
+        if p[i] <= a {
+            count[0] += 1;
+        } else if a < p[i] && p[i] <= b {
+            count[1] += 1;
+        } else {
+            count[2] += 1;
+        }
+    }
+
+    println!("{}", count.iter().min().unwrap())
 }
