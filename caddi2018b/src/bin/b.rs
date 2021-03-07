b6cd87a9086bd3aac5a6a6902c0a49c7ee259a13
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-07 15:33:16 +0900

    caddi2018b-b

--- caddi2018b/src/bin/b.rs
+++ caddi2018b/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: usize,
+        w: usize,
+        abs: [(usize, usize); n],
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        if h <= abs[i].0 && w <= abs[i].1 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
