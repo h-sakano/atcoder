366625941cd4422272b7ef366b438e115a3ac143
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-09 21:56:28 +0900

    ABC192-B

--- abc192/src/bin/b.rs
+++ abc192/src/bin/b.rs
@@ -1,6 +1,24 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    for i in 0..s.len() {
+        if i % 2 == 0 {
+            if s[i].is_uppercase() {
+                println!("No");
+                return;
+            }
+        } else {
+            if s[i].is_lowercase() {
+                println!("No");
+                return;
+            }
+        }
+    }
+
+    println!("Yes");
 }
