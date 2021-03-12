cabba863d0db3fdbff1e78c09aea0f0eca6d7e54
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-12 23:34:25 +0900

    abc192-C

--- abc192/src/bin/c.rs
+++ abc192/src/bin/c.rs
@@ -1,6 +1,33 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        k: usize,
+    }
+
+    let mut a = n;
+
+    for _ in 0..k {
+        let g1: i64 = a
+            .to_string()
+            .chars()
+            .sorted()
+            .rev()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        let g2: i64 = a
+            .to_string()
+            .chars()
+            .sorted()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        a = g1 - g2;
+    }
+
+    println!("{}", a);
 }
