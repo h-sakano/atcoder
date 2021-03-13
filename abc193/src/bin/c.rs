bf26ecfe37324c7461ad3f1323794d5646ef3dd3
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-13 18:46:27 +0900

    abc193-c

--- abc193/src/bin/c.rs
+++ abc193/src/bin/c.rs
@@ -1,6 +1,20 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut set = HashSet::new();
+    for i in 2..=((n as f64).sqrt().floor() as usize) {
+        let mut now = i * i;
+        while now <= n {
+            set.insert(now);
+            now *= i;
+        }
+    }
+
+    println!("{}", n - set.len());
 }
