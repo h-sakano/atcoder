f3eaed846c9b08e1cea02c4c4e6b80e014eaac4b
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-18 16:20:11 +0900

    abc194-b

--- abc194/src/bin/b.rs
+++ abc194/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        abs: [(usize, usize); n],
+    }
+
+    let mut ans = 1_000_000;
+    for i in 0..n {
+        let a = abs[i].0;
+        for j in 0..n {
+            let b = abs[j].1;
+            let mut time = std::cmp::max(a, b);
+            if i == j {
+                time = a + b;
+            }
+            ans = std::cmp::min(ans, time);
+        }
+    }
+
+    println!("{}", ans);
 }
