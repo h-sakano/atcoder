0b7c42e9c7354c3842754722ec5f16e2f2627798
Hiroki Sakano <h.sakano26@gmail.com>
2021-03-18 17:04:09 +0900

    abc194-c

--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut dict = vec![0; 401];
+    for i in 0..n {
+        dict[(a[i] + 200) as usize] += 1;
+    }
+
+    let mut ans = 0;
+    for i in 0..=399 {
+        let mut tmp = 0;
+        for j in i + 1..=400 {
+            tmp += (j - i) * (j - i) * dict[j];
+        }
+        ans += tmp * dict[i];
+    }
+
+    println!("{}", ans);
 }
